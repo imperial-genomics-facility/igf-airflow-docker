bd360edb5375427b4ce1709f984156e772f2bfd8
avik <reach4avik@yahoo.com>
2019-02-24 22:38:35 +0000

    Update entrypoint.sh

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -50,10 +50,8 @@ wait_for_port() {
 
 ## Check for Postgres db
 
-if [[ "$AIRFLOW__CORE__EXECUTOR" != "SequentialExecutor" ]]; then
-  if [[ -z "$AIRFLOW__CORE__SQL_ALCHEMY_CONN" ]]; then 
-    AIRFLOW__CORE__SQL_ALCHEMY_CONN="postgresql+psycopg2://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
-  fi
+if [[ "$AIRFLOW__CORE__EXECUTOR" != "SequentialExecutor" && -z "$AIRFLOW__CORE__SQL_ALCHEMY_CONN" ]]; then 
+  AIRFLOW__CORE__SQL_ALCHEMY_CONN="postgresql+psycopg2://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
   wait_for_port "Postgres" "$POSTGRES_HOST" "$POSTGRES_PORT"
 fi
 
